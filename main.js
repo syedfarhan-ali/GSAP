b8ccdf34c4f0e05ed0c0fe8d932f61ba229b730b
syedfarhan-ali <syedfarhanali012005@gmail.com>
2026-04-12 13:12:08 +0530

    added gsap.to gsap.from yoyo and repeat

---
+++ main.js
@@ -0,0 +1,36 @@
+// gsap.to('#box', {
+//   x:1000,
+//   duration:1,
+//   delay:1,
+  
+// })
+// gsap.to('#box2', {
+//   x:1000,
+//   duration:1,
+//   delay:1,
+  
+// })
+// gsap.from('#box', {
+//   x:1000,
+//   duration:1,
+//   delay:1,
+//   rotate:360,
+//   backgroundColor:'blue'
+// })
+// gsap.to('#box', {
+//   x:1000,
+//   duration:1,
+//   delay:1,
+//   rotate:360,
+//   backgroundColor:'blue',
+//   borderRadius:'100%'
+// })
+
+gsap.from('h1', {
+  opacity:0,
+  duration:2,
+  delay:0.5,
+  yoyo:true,
+  repeat:-1,
+  stagger:0.3
+})
