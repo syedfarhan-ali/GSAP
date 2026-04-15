547f077d0fe5089cc0fe231029ca2322ddea9c12
syedfarhan-ali <syedfarhanali012005@gmail.com>
2026-04-15 15:40:20 +0530

    added svg animations

---
+++ GSAP_SVGAnimations/main.js
@@ -0,0 +1,20 @@
+let finalPath = "M 10 100 Q 500 100 990 100"
+
+let string = document.querySelector("#string")
+
+string.addEventListener("mousemove", (dets) => {
+  path = `M 10 100 Q ${dets.x} ${dets.y} 990 100`
+  gsap.to("svg path", {
+    attr:{d:path},
+    duration:0.2,
+    ease:"power3.out"
+  })
+})
+
+string.addEventListener("mouseleave", (dets) => {
+  gsap.to("svg path", {
+    attr:{d:finalPath},
+    duration:1.5,
+    ease:"elastic.out(1,0.2)"
+  })
+})
